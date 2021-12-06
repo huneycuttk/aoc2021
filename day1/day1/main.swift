96b3c034060017f1b4d4bd2e118c2584ee23a859
Karsten Huneycutt <kph@pobox.com>
2021-12-06 04:25:59 -0500

    cleanup

--- day1/day1/main.swift
+++ day1/day1/main.swift
@@ -8,33 +8,35 @@
 import Foundation
 
 
-let testData = [ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-let testAnswerOne = 7
-guard testAnswerOne == countIncreasing(testData) else {
-    print("countIncreasing is wrong!!")
-    exit(1)
-}
+let testDepths = [ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+let testIncreasingDepths = countIncreasing(testDepths)
+assert(testIncreasingDepths == 7)
 
 let url = URL(string: "file:///Users/kph/Stuff/aoc2021/day1/input.txt")!
 let depths = try String(contentsOf: url).split(separator: "\n").map { Int($0)! }
 
 let increasingDepths = countIncreasing(depths)
-
 print("Count increasing depths answer is \(increasingDepths)")
+assert(increasingDepths == 1713)
 
-let windows = triplewise(depths).map { t -> Int in
-    let (a,b,c) = t
-    return a+b+c
-}
+let testIncreasingWindows = countIncreasing(windows(testDepths))
+assert(testIncreasingWindows == 5)
 
-let increasingWindows = countIncreasing(windows)
-
+let increasingWindows = countIncreasing(windows(depths))
 print("Count increasing windows answer is \(increasingWindows)")
+assert(increasingWindows == 1734)
 
 func countIncreasing(_ ary: [Int]) -> Int {
     return zip(ary, ary.dropFirst()).reduce(0) { count, tuple in
         let (a, b) = tuple
         return b > a ? count + 1 : count
+    }
+}
+
+func windows(_ ary: [Int]) -> [Int] {
+    return triplewise(ary).map { t -> Int in
+        let (a,b,c) = t
+        return a+b+c
     }
 }
 
